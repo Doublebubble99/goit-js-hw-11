04c8b59776ff879b5b3ab9fd7e503b25306b4b33
Artemon Chik <zagrousaspear@gmail.com>
2023-04-18 20:34:51 +0200

    complete hw-11 v3.6

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import Notiflix from 'notiflix';
-import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { getImages } from '../src/GetImages';
-var axios = require('axios/dist/browser/axios.cjs');
 var lightbox = new SimpleLightbox('.gallery a', {
   animationSpeed: 250,
   fadeSpeed: 250,
 });
 export let page = 1;
 export let per_page = 40;
+const lastPage = 13;
 const refs = {
   searchForm: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
   input: document.querySelector('input'),
   loadMore: document.querySelector('.load-more'),
 };
-export const { searchForm, gallery, input, loadMore } = refs;
+const { searchForm, gallery, input, loadMore } = refs;
 lightbox.on('show.simplelightbox', () => {
   lightbox.shown();
 });
@@ -25,25 +24,38 @@ function reset() {
   gallery.innerHTML = '';
 }
 loadMore.classList.add('is-hidden');
-searchForm.addEventListener('submit', evt => {
+searchForm.addEventListener('submit', async evt => {
   evt.preventDefault();
   reset();
   loadMore.classList.add('is-hidden');
-  queryPictures(input.value.trim());
+  await queryPictures(input.value.trim());
 });
-loadMore.addEventListener('click', () => {
-  getMoreContent();
-});
-export async function getMoreContent() {
+loadMore.addEventListener('click', async () => {
   page += 1;
-  loadMore.classList.remove('is-hidden');
-  renderPageByName(await getImages(input.value.trim()));
-}
-export async function queryPictures(name) {
+  await getMoreContent(input.value.trim());
+});
+async function getMoreContent(name) {
   renderPageByName(await getImages(name));
 }
-export async function renderPageByName(cards) {
-  const cardList = await cards
+async function queryPictures(name) {
+  if (!name) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    loadMore.classList.add('is-hidden');
+    return;
+  }
+  renderPageByName(await getImages(name));
+}
+function renderPageByName(cards) {
+  if (cards.hits.length === 0) {
+    loadMore.classList.add('is-hidden');
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  const cardList = cards.hits
     .map(
       ({
         webformatURL,
@@ -75,5 +87,20 @@ export async function renderPageByName(cards) {
     )
     .join('');
   gallery.insertAdjacentHTML('beforeend', cardList);
+
+  if (cards.hits.length < 40 && page === 1) {
+    loadMore.classList.add('is-hidden');
+    return;
+  } else if (page === lastPage || cards.hits.length < 40) {
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+    loadMore.classList.add('is-hidden');
+    lightbox.refresh();
+    return;
+  } else if (page === 1) {
+    Notiflix.Notify.success(`Hooray! We found ${cards.totalHits} images.`);
+  }
   lightbox.refresh();
+  loadMore.classList.remove('is-hidden');
 }
